61d0a5e9006c9003ad12fe759208f63351206dc9
Jeff DiNoto <jeff.dinoto@gmail.com>
2017-08-01 09:02:43 -0400

    Updating test file.

--- dist/test/test.js
+++ dist/test/test.js
@@ -1,9 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var disney_parks_calendar_1 = require("../disney-parks-calendar");
-var date0 = new Date("2017-07-23 00:00:01");
-var date1 = new Date("2017-07-24 00:00:01");
-var dates = [date0, date1];
+var date0 = new Date("2017-07-04 00:00:01");
+var dates = [date0];
 disney_parks_calendar_1.DisneyParksCalendar.getParkHours.apply(disney_parks_calendar_1.DisneyParksCalendar, dates).toArray().
     subscribe(function (data) { return data.forEach(function (operatingHours) { return console.log(operatingHours.toString()); }); }, function (error) { return console.error(error); });
 //# sourceMappingURL=test.js.map
--- src/test/test.ts
+++ src/test/test.ts
@@ -1,8 +1,7 @@
 import { DisneyParksCalendar } from '../disney-parks-calendar';
 
-let date0: Date = new Date("2017-07-23 00:00:01");
-let date1: Date = new Date("2017-07-24 00:00:01");
-let dates: Array<Date> = [date0, date1];
+let date0: Date = new Date("2017-07-04 00:00:01");
+let dates: Array<Date> = [date0];
 DisneyParksCalendar.getParkHours(...dates).
     toArray().
     subscribe(
